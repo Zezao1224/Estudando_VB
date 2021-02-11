6edf039918d741c2ab40234a92483ca89cd963af
Bruno Luiz <gamer@PCZezao.sa-east-1.ec2-utilities.amazonaws.com>
2021-02-11 17:39:27 -0300

    primeira_versaoVB

---
+++ Estudo/Conexao.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+Imports Aerospike.Client
+
+Public Class Conexao
+    Dim StrlCaminho As String = "C:\Users\gamer\Documents\Database2.accdb"
+    Dim cn As OleDbConnection
+    Sub Conect()
+        Dim Testeconect As Boolean
+        cn = New OleDbConnection("PROVIDER=Microsoft.ACE.OLEDB.12.0;DATA Source=" & StrlCaminho & "")
+        Try
+            cn.Open()
+            Testeconect = True
+        Catch ex As Exception
+            Testeconect = False
+        End Try
+        If Testeconect = True Then
+        Else
+            Console.WriteLine("result error ")
+        End If
+        Call Table()
+        'cn.Close()
+    End Sub
+    Sub Table()
+        Dim Table As New OleDbCommand
+        Dim clientesReader As OleDbDataReader
+        Dim i As Integer = 0
+        Table.Connection = cn
+        Table.CommandText = "SELECT TableSoares.* FROM TableSoares;"
+        clientesReader = Table.ExecuteReader()
+        Try
+            Dim readerTemRegistros As Boolean = clientesReader.Read
+            While readerTemRegistros = True
+                Console.WriteLine("Nome cabloco: " & clientesReader.GetValue(1).ToString)
+                Console.WriteLine("Idade: " & clientesReader.GetValue(2).ToString)
+                readerTemRegistros = clientesReader.Read()
+            End While
+        Catch ex As Exception
+
+        End Try
+        cn.Close()
+    End Sub
+End Class
---
+++ Estudo/Program.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Start Conect")
+        Dim Conexao As New Conexao
+        Conexao.Conect()
+    End Sub
+End Module
